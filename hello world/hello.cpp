e54b7e7fb9cc6199d7cc4e246bd47b6b657d9973
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2026-04-09 14:14:43 +0600

    Added all C++ projects

---
+++ hello world/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    int num1=41;
+    int num2=14;
+    int sum=num1+num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+    return 0;
+}

b6b6a7d80ced9bc5892d93093dd1a4efa2989b08
Md Mahmud Ul Hasan <mahmudulhasananik40@gmail.com>
2026-04-14 00:37:24 +0600

    Added all C++ programs

---
+++ function/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int sum(int a, int b)
+{
+    int x=a+b;
+    return x;
+}
+
+int main()
+{
+   int x=10, y=15;
+   cout<< sum(x,y)<<endl;
+    return 0;
+}
